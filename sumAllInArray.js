d5c031ab24d73fcef3d0b23118354b4d0308b60e
James Mernin <jamespmernin@gmail.com>
2020-09-30 13:57:04 -0400

    Algorithm solved

--- sumAllInArray.js
+++ sumAllInArray.js
@@ -1,7 +1,14 @@
 //SUM ALL IN ARRAY
 
 const sumAllInArray = arr => {
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let num = parseInt(arr[i]);
+    if (!isNaN(num)) {
+      sum += num;
+    }
+  }
+  return sum;
 }
 
 console.log(sumAllInArray(["-1", "a", 4, "-32", 8, "94"]))
